adece9fe27100a0a91fae8f2a6921ada097695eb
juniorvieiradahora-crypto <juniorvieiradahora@gmail.com>
2026-05-09 16:53:42 -0300

    Add files via upload

---
+++ entrada_05.py
@@ -0,0 +1,18 @@
+# Autor : driano vieira
+# projeto : entrada com input e F - string
+
+# declaração de variaveis
+nome = input("digite seu nome")
+valor1 = int(input('digite o primeiro valor: '))
+valor2 = int(input('digite o segundo valor: '))
+soma = valor1 + valor2
+subtracao = valor1 + valor2
+multiplicacao = valor1 + valor2
+divisao = valor1 + valor2
+
+# exibindo os resultados com F - string 
+print(nome)
+print('o resultado da soma é: '{soma})
+print('o resultado da soma é: ',{subtracao})
+print('o resultado da soma é: ',{multiplicacao})
+print('o resultado da soma é: ',{divisao})
