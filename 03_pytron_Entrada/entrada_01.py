8db08cdd038f5fb5df0307f01529fde783ba126a
juniorvieiradahora-crypto <juniorvieiradahora@gmail.com>
2026-05-09 15:00:23 -0300

    entrada em python

---
+++ 03_pytron_Entrada/entrada_01.py
@@ -0,0 +1,10 @@
+# Autor: adriano vieira
+# pojeto: entradas em python
+
+# declarar variaveis 
+valor1 = 6
+valor2 = 2
+soma = valor1 + valor2
+
+# exibir resultados
+print('a soma é: ', soma)
---
+++ 03_pytron_Entrada/entrada_02.py
@@ -0,0 +1,10 @@
+# Autor : driano vieira
+# projeto : entrada com input
+
+# declaração de variaveis
+valor1 = int(input('digite o primeiro valor: '))
+valor2 = int(input('digite o segundo valor: '))
+soma = valor1 + valor2
+
+# exibindo os resultados 
+print('o resultado da soma é: ',soma)
